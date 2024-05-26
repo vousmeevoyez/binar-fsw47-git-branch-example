e504ad65cabc9e14a9f56bf39023be5080405925
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-05-26 21:39:18 +0700

    First Commit

---
+++ functionDeclaration.js
@@ -0,0 +1,22 @@
+function discountSteamSale(price, discountPercentage) {
+  return price - price * (discountPercentage / 100);
+}
+
+function summerSale(price) {
+  const summerDiscount = 20; // Contoh diskon Summer Sale 20%
+  return discountSteamSale(price, summerDiscount);
+}
+
+function winterSale(price) {
+  const winterDiscount = 30; // Contoh diskon Winter Sale 30%
+  return discountSteamSale(price, winterDiscount);
+}
+
+// Contoh penggunaan
+let originalPrice = 50; // Misal harga asli game adalah $50
+
+let summerSalePrice = summerSale(originalPrice);
+console.log(`Harga setelah Summer Sale: $${summerSalePrice}`); // Harga setelah diskon Summer Sale
+
+let winterSalePrice = winterSale(originalPrice);
+console.log(`Harga setelah Winter Sale: $${winterSalePrice}`); // Harga setelah diskon Winter Sale
