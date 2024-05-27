0301527cbd2e7926025aa35993e8aa99e23d3b77
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-05-27 08:02:17 +0700

    tambah materi topic 3

---
+++ bingleFunctionCase.js
@@ -0,0 +1,29 @@
+function greetings(username) {
+  console.log(
+    "Selamat datang " + username + " di Bingle, Let's start your journey!"
+  );
+}
+
+greetings("Farhan Adi S");
+
+function diskonBirthday(x) {
+  let birthdaySale = (x * 30) / 100;
+  return birthdaySale;
+}
+
+let sale = diskonBirthday(25000);
+console.log(sale);
+
+//Kode 1 function declaration
+test();
+
+function test() {
+  console.log("it works");
+}
+
+//Kode 2 function expression
+const test2 = function () {
+  console.log("it works");
+};
+
+test2();
