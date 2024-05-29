9981ce80928ffc97337e8dac1f7204bc6008d356
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-05-29 19:26:25 +0700

    cleaning

--- forloop.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from "readline-sync";
-
-const lantaiLevel = readlineSync.question("Mau ngepel berapa lantai? (1-100)");
-
-for (let i = 1; i <= lantaiLevel; i++) {
-  console.log(i);
-  console.log(`lagi ngepel lantai ${i}`);
-}
--- recipeTenary.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from "readline-sync";
-
-const suhu = readlineSync.question("Berapa suhu oven? dalam celcius");
-
-const isPanas = suhu < 100 ? "tidak panas" : "panas";
-console.log(isPanas);
-
-console.log(suhu < 100 ? "tidak panas" : "panas");
